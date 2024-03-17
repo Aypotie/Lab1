aa737776922eee501b88f00e13ad85563cd69757
Dashiiidzzze <ejikevpsihiy@gmail.com>
2024-03-18 00:50:21 +0700

    меню треугольника

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const input = require('prompt-sync')();
 const { trapezoidArea, trapezoidMiddleLine, trapezoidPerimeter } = require('./trapezoid.js')
+const triangle = require('./triangle.js');
 
 const menu = "=== MENU ===\n1. triangle\n2. trapezoid\n3. break\n============";
 
@@ -8,7 +9,14 @@ while (true) {
 
     let choice = input('Введите фигуру: ');
     if (choice == 1){
-        // TODO: create triangle
+        let aTriangle = Number(input('Введите основание трецгольника:'));
+        let bTriangle = Number(input('Введите левую сторону треугольника:'));
+        let cTriangle = Number(input('Введите правую сторону треугольника:'));
+        if (aTriangle + bTriangle > cTriangle && aTriangle + cTriangle > bTriangle && cTriangle + bTriangle > aTriangle){
+            console.log('Периметр треугольника:', triangle.perimeter(aTriangle, bTriangle, cTriangle));
+            console.log('Площадь треугольника:', triangle.area(aTriangle, bTriangle, cTriangle));
+            console.log('проверка на равнобедренность треугольника:', triangle.isosceles(aTriangle, bTriangle, cTriangle));
+        } else console.log('Треугольника задана неправильно');
     } else if (choice == 2){
         let a = Number(input('Введите большее основание трапеции:'));
         let b = Number(input('Введите меньшее основание трапеции:'));
